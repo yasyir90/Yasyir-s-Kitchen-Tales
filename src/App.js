6818b4dfe94f7430ee6f3fc0db74e9ddb036d373
yasyir90 <yasyirmasyal@gmail.com>
2023-05-04 07:07:16 +0700

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import NavbarH from './components/navbar/navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <NavbarH/>
     </div>
   );
 }
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,66 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import './navbar.css'
+import logo from './logo.png'
+
+function NavbarH() {
+  return (
+    <Navbar expand="lg" sticky="top" style={{backgroundColor: '#3c4a3e'}} >
+    <Container fluid className="px-lg-5">
+      <Navbar.Toggle aria-controls="navbarSupportContent" id="togle" />
+      <Navbar.Collapse id="navbarSupportContent">
+        <Nav className="link"  defaultActiveKey="/">
+        <Nav.Link  to="/">
+              <img src={logo} alt='logo' style={{ width: '35px',}}/>
+            </Nav.Link>
+          <Nav.Link  to="/">
+            Home
+          </Nav.Link>
+          <Nav.Link  to="/allfood">
+            All Food
+          </Nav.Link>
+          {localStorage.getItem("token") && (
+            <Nav.Link  to="/favorite">
+              Favorite
+            </Nav.Link>
+          )}
+          {localStorage.getItem("role") === "admin" && (
+            <Nav.Link  to="/add-food">
+              Add Food
+            </Nav.Link>
+          )}
+        </Nav>
+        <Navbar.Brand  to="/">
+          <span className="mx-1 logo fs-1 text-focus-in">Yasyir's Kitchen Tales</span>
+        </Navbar.Brand>
+        <Nav className="link">
+          {localStorage.getItem("token") ? (
+            <NavDropdown id="basic-nav-dropdown drp">
+              <NavDropdown.Item  to={`/profile`}>
+                My Profile
+              </NavDropdown.Item>
+              {localStorage.getItem("role") === "admin" && (
+                <NavDropdown.Item  to="/all-users">
+                  All User
+                </NavDropdown.Item>
+              )}
+              <NavDropdown.Divider />
+              <NavDropdown.Item >
+                Logout
+              </NavDropdown.Item>
+            </NavDropdown>
+          ) : (
+            <Nav.Link  to="/login" className="log">
+              Login
+            </Nav.Link>
+          )}
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+  );
+}
+
+export default NavbarH;
